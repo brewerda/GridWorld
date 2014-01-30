942303c22b3e95f0bd060da3955220f4a9ea00b6
brewerda <brewerda@s.dcsdk12.org>
2014-01-29 22:36:26 -0700

    Update ChameleonKid.java

--- critters/ChameleonKid.java
+++ critters/ChameleonKid.java
@@ -17,10 +17,17 @@ public class ChameleonKid extends ChameleonCritter{
 	return a;
 	}
 	   
-
-	public void makeMove(Location loc)
-	{
-	    setDirection(getLocation().getDirectionToward(loc));
-	    super.makeMove(loc);
-	}
-}
+ 	public ArrayList<Location> getLocationsInDirections(int[] directions) { 
+ 	ArrayList<Location> locs = new ArrayList<Location>(); 
+ 	Grid gr = getGrid(); 
+ 	Location loc = getLocation(); 
+ 
+	 	for (int d : directions) { 
+ 		Location neighborLoc = loc.getAdjacentLocation(getDirection() + d); 
+	 		if (gr.isValid(neighborLoc)) {
+ 			locs.add(neighborLoc); 
+			 }
+ 		} 
+ 	return locs; 
+ 	} 
+} 
