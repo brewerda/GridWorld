6bf256b33bb90772b8234d37735890d2c06cc330
brewerda <brewerda@s.dcsdk12.org>
2014-01-31 19:54:56 -0700

    Update KingCrab.java

--- critters/KingCrab.java
+++ critters/KingCrab.java
@@ -1,7 +1,26 @@
 public class KingCrab extends CrabCritter {
 
 
-  public void moveAway(Actor a) {
+  public boolean moveAway(Actor a) {
   
     ArrayList<Location> locs = getGrid().getEmptyAdjacentLocations(a.getLocation());
+    
+    
+    for (int i=0; i<=locs.size(); i++) {
+      if(distanceFrom(getLocation(), loc) > 1) {
+        a.moveTo(loc);
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  
+  public void processActors(ArrayList<Actor> actors) {
+    
+    for(int a=0; a<=actors.size(); a++) {
+      if(moveAway(a) == false) {
+        a.removeSelfFromGrid();
+      }
+    }
     
